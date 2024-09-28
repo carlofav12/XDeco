e3856793f7c8d942e5d020356e4af3d54a4d5772
Adriancas28 <castroadrian1228@gmail.com>
2024-09-28 18:17:00 -0500

    creando la Rama Contacto

---
+++ Models/Contacto.cs
@@ -0,0 +1,29 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace XDeco.Models
+{
+    [Table("t_contacto")]
+    public class Contacto
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Titulo { get; set; }
+
+        [Required]
+        public string Mensaje { get; set; }
+
+        [Phone]
+        public string? Telefono { get; set; }
+
+        public string? Direccion { get; set; }
+    }
+}
