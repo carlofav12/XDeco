35edc54398ce7010a9a2dd8e6898564d70e5b609
Jhowill <jhowillsc@gmail.com>
2024-09-28 18:02:33 -0500

    Conexion al PostgreSQL

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ using Proyecto.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("PosgreConnection") 
+    ?? throw new InvalidOperationException("Connection string 'PosgreConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
+    
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
