d1a940a9df3d2c70c1b31e869483a509ba20b27f
Adriancas28 <castroadrian1228@gmail.com>
2024-10-01 15:24:19 -0500

    merge Administrador Controller

---
+++ Controllers/AdministradorController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace XDeco.Controllers
+{
+    [Route("[controller]")]
+    public class AdministradorController : Controller
+    {
+        private readonly ILogger<AdministradorController> _logger;
+
+        public AdministradorController(ILogger<AdministradorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
