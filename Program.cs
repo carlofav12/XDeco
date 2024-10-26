b5ad3952e4bdc6bf8237356de70174af1b8bc919
Usmpmarkp <sebasklidad107@gmail.com>
2024-10-25 22:33:20 -0500

    frase del dia

--- Program.cs
+++ Program.cs
@@ -8,34 +8,45 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Obtener la cadena de conexión
 var connectionString = builder.Configuration.GetConnectionString("PosgreConnection")
     ?? throw new InvalidOperationException("Connection string 'PosgreConnection' not found.");
+
+// Configurar DbContext con PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Configurar el filtro para excepciones de desarrollo
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// Configurar identidad y opciones de usuario
 builder.Services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+// Registro de servicios personalizados
 builder.Services.AddSingleton<EmailService, EmailService>();
-builder.Services.AddControllersWithViews();
-
 builder.Services.AddScoped<NYTimesApiIntegration>();
 
+// Configurar controladores, vistas y API Explorer
+builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen( c =>
+
+// Configurar Swagger para la documentación de API
+builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo{ Title = "XDeco Api", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "XDeco Api", Version = "v1" });
 });
+
+// Registrar HttpClient
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline de la aplicación
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI( c =>
+    app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "XDeco Api V1");
     });
@@ -44,20 +55,15 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
+// Configuración de rutas
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
