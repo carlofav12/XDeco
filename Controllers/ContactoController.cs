1c477d162d7322da8f55df6fd57fc9ace83d0c97
carlofav12 <carlofav12@gmail.com>
2024-11-01 18:20:28 -0500

    Mensaje de clasificacion en contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -1,13 +1,12 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Proyecto.Data;
 using XDeco.Models;
+using Ml_xdeco;
+using static Ml_xdeco.MLtextclasification;
 
 namespace XDeco.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // Cambia a una ruta más específica si es necesario
     public class ContactoController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -17,28 +16,48 @@ namespace XDeco.Controllers
             _context = context;
         }
 
-        // GET: Contacto/Create
-        [HttpGet]
+        // GET: contacto/index
+        [HttpGet("index")]
         public IActionResult Index()
         {
-            return View();
+            return View(); 
         }
 
-        // POST: Contacto/Create
+        // POST: contacto/create
         [HttpPost("create")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Contacto objcontato)
         {
-            if (ModelState.IsValid) // Verifica si el modelo es válido
+            if (!ModelState.IsValid) 
             {
-                _context.Add(objcontato);
-                await _context.SaveChangesAsync();
-
-                ViewData["Message"] = "Se registró el contacto";
-                return RedirectToAction("Index"); // Redirige a Index para evitar reenvío de formulario
+                return View("Index", objcontato);
             }
 
-            return View("Index", objcontato); // Devuelve a la vista Index con el modelo si hay errores
+            // Agregar el contacto a la base de datos
+            _context.Add(objcontato);
+            await _context.SaveChangesAsync();
+
+            // Realizar la predicción utilizando el modelo ML.NET
+            var input = new ModelInput { Mensaje = objcontato.Mensaje };
+            var result = MLtextclasification.Predict(input); // Llama al método Predict
+
+            // Determinar el mensaje según la predicción
+            string mensajeClasificacion;
+            if (result.PredictedLabel == 0)
+            {
+                mensajeClasificacion = "Gracias por su contacto.";
+            }
+            else if (result.PredictedLabel == 1)
+            {
+                mensajeClasificacion = "Estamos intentando mejorar. Su opinión es importante.";
+            }
+            else
+            {
+                mensajeClasificacion = "Clasificación desconocida.";
+            }
+
+            TempData["Message"] = mensajeClasificacion; // Almacena el mensaje en TempData
+            return RedirectToAction("Index");
         }
     }
 }
