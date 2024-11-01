2535631f75dc06914a744953bac109cb1075857c
Usmpmarkp <sebasklidad107@gmail.com>
2024-11-01 17:28:40 -0500

    cambio en el mlnet

--- ML/textclasification/MLtextclasification.consumption.cs
+++ ML/textclasification/MLtextclasification.consumption.cs
@@ -52,7 +52,7 @@ namespace Ml_xdeco
 
         #endregion
 
-        private static string MLNetModelPath = Path.GetFullPath("MLModel1.mlnet");
+        private static string MLNetModelPath = Path.GetFullPath("MLtextclasification.mlnet");
 
         public static readonly Lazy<PredictionEngine<ModelInput, ModelOutput>> PredictEngine = new Lazy<PredictionEngine<ModelInput, ModelOutput>>(() => CreatePredictEngine(), true);
 
